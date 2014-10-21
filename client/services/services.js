24dde4e65d9df3ae262f2533d4622dd055b575bf
Kia Winslow <ksukop@gmail.com>
2014-10-20 22:07:30 -0700

    on the way to the sky

--- client/services/services.js
+++ client/services/services.js
@@ -1,11 +1,61 @@
 angular.module('services', [])
 
+.factory('Query', function($q, $http, Location, Twitter, Instagram){
+
+  var getData = function(request){
+
+    $scope.loading = false;
+
+    var params = {
+      street: request.street,
+      city: request.city,
+      state: request.state
+    };
+
+    return Location.getLocation(params)
+      .then(function(coords){
+        var tweetParams = {
+          query: request.query,
+          latitude: coords.latitude,
+          longitude: coords.longitude //!!! need to add date and radius
+        };
+
+        return Twitter.getTweets(tweetParams)
+          .then(function(tweetData) {
+            $scope.data.tweets = tweetData;
+            return $scope.data.tweets;
+          })
+          .catch(function(error){
+            console.error(error);
+          })
+      })
+
+      //finish it when get info about Insta API
+      //   var instaParams = {
+
+      //   }
+      //   Instagram.getPhotos()
+      //     .then(function(photoData) {
+      //       $scope.data.photos = photoData;
+      //     })
+      // })
+      .catch(function(error){
+        console.error(error);
+      })
+  }
+
+  return {
+    getData: getData
+  }
+})
+
 .factory('Twitter', function($http) {
 
   var getTweets = function(request) {
     return $http({
       method: 'GET',
-      url: 'api/twitter'
+      url: 'api/twitter',
+      params: request
     })
     .then(function(response) {
       console.log('response.data');
@@ -50,7 +100,7 @@ angular.module('services', [])
     //creating the object with request for this service
     params = {
       query: request,
-      location: 'MN', // !!! need to change after getting the location from user
+      location: request.city, //!!! need to add logic of inserting the location field depending on user input (if there is only state, put state, if there is no location, search for all US,  etc.)
       amount: 5
     };
     return $http({
@@ -73,7 +123,7 @@ angular.module('services', [])
 })
 
 // converts a street address into geo coordinates, needed for instagram & twitter apis
-.factory('GetLocation', function($http) {
+.factory('Location', function($http) {
   var getLocation = function(request) {
     var params = {
       street: request.street,
@@ -87,11 +137,17 @@ angular.module('services', [])
       params: params
     })
     .then(function(response) {
-      var lattitude = response.results[0].geometry.location.lat;
+      var latitude = response.results[0].geometry.location.lat;
       var longitude = response.results[0].geometry.location.lng;
-      var coords = [lattitude, longitude];
+      var coords = {
+        latitude: latitude,
+        longitude: longitude
+      };
       console.log(coords);
       return coords;
+    })
+    .catch(function(error){
+      console.error(error);
     });
   };
 
